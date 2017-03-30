0fa5bc9ce42a851bfe55cdefd1ffe5b6310c85ea
Adam Erispaha <aerispaha@gmail.com>
2017-03-29 21:55:49 -0400

    flow splitting and limiting sewer logic

--- sewertrace/trace.py
+++ sewertrace/trace.py
@@ -15,16 +15,24 @@ def upstream_accumulate_all(G, parameter='Shape_Area'):
     tributaries have/haven't been traversed.
     """
 
-    #keep track of where flow splits are observed
-    splitnodes = []
     G1 = G.copy()
+
+    for n in nx.topological_sort(G1):
+        #loop through each and accumulate area with its immediate
+        #upstream nodes. where there's a split, apply the split fraction
+        area = sum(get_node_values(G1, [n], 'Shape_Area')) / 43560.0
+        for p in G1.predecessors(n):
+            area += G1.node[p]['total_area_ac']
+            if 'flow_split_frac' in G1[p][n]:
+                area *= G1[p][n]['flow_split_frac']
+
+        G1.node[n]['total_area_ac'] = area
 
     for u,v,d in G1.edges_iter(data=True):
 
         #collect the upstream nodes, sum area
         up_nodes = nx.ancestors(G1, u) | set({u}) #upstream nodes
-        areas = get_node_values(G1, up_nodes, 'Shape_Area')
-        acres = sum(areas) / 43560.0
+        acres = G1.node[v]['total_area_ac']
 
         #find path and calculate tc (sum trave)
         tc_path = find_tc_path(G1, u, 'travel_time')
@@ -53,27 +61,7 @@ def upstream_accumulate_all(G, parameter='Shape_Area'):
         [d.pop(k, None) for k in rmkeys]
 
     return G1
-    
-def upstream_accumulate(G, node, parameter=None):
-    """
-    sum of the values of each upstream manhole's parameter value,
-    from the given node.
-    """
 
-    #return a set of the upstream nodes, including the current node
-    upstream_nodes = nx.ancestors(G, node) | set({node})
-
-    #find any flow splits
-    splits = flow_splits(G)
-
-    if parameter is None:
-        upstream_count = [1 for n in upstream_nodes if 'Depth' in G.node[n]]
-        return sum(upstream_count)
-
-    upstream_vals = [G.node[n][parameter] for n in upstream_nodes
-                     if parameter in G.node[n]]
-
-    return sum(upstream_vals)
 
 def get_node_values(G, nodes, parameter):
     """return a list of values in nodes having the parameter"""
@@ -101,46 +89,87 @@ def find_tc_path(G, start_node=None, parameter='length'):
     tc_path, longest_len = [], 0
     for n in top_nodes:
         for path in nx.all_simple_paths(G2, source=n, target=start_node):
-            path_len = sum([G2[u][v][parameter] for u, v in pairwise(path)])
+            path_len = sum([G2[u][v][parameter] for u,v in pairwise(path)])
             if path_len > longest_len:
                 tc_path = path
                 longest_len = path_len
 
     return tc_path
 
-def flow_splits(G):
+def analyze_downstream(G):
+    """
+    """
+    G1 = G.copy()
+    start_node = nx.topological_sort(G1, reverse=True)[0]
+    for u,v,d in G1.edges_iter(data=True):
+        descendants = []
+        rates = []
+        for path in nx.all_simple_paths(G1, source=v, target=start_node):
 
-    splitnodes = []
-    for n, d in G.nodes_iter(data=True):
-        if G.out_degree(n) > 1:
-            #node has more than one successor, may be a flow split
-            if 'Subtype' in G.node[n] and G.node[n]['Subtype'] != 6:
+            #descendants = nx.descendants(G1, u)
+            descendants += [G1[u][v] for u,v in pairwise(path)]
+            rates += [(e['phs_rate'], e['FACILITYID']) for e in descendants]
 
-                G.node[n]['flow_split_keys']={}
-                #subtype = 6 is a summit manhole
-                splitnodes.append(n)
+        if descendants:
+            sorted_rates = sorted(rates)
+            d['descendants'] = [e['FACILITYID'] for e in descendants]
+            d['limiting_rate'], d['limiting_sewer'] = sorted_rates[0]
+
+    return G1
+
+def analyze_flow_splits(G):
+
+    """
+    loop through nodes, find nodes with more than 1 outflow (flow split)
+    tag the immediately downstream sewers as flow splitters
+    find the resolving node by finding the closest downstream node with
+    mulitple inflows and more than one path from the splitter to resolver
+    """
+
+    G1 = G.copy()
+
+    #iterate through nodes having more than one out degree
+    splitters = [(n, deg) for n, deg in G1.out_degree_iter() if deg > 1]
+    for splitter, out_degree in splitters:
+
+        #find the where the split is resolved
+        candidates = [m for m in nx.descendants(G1, splitter)
+                      if G1.in_degree(m) > 1]
+
+        #find nodes that have mulitple paths connecting to split node
+        resolver_paths = []
+        for r in candidates:
+            paths = list(nx.all_simple_paths(G1, source=splitter, target=r))
+            if len(paths) > 1:
+                resolver_paths += paths
+
+        #find the shortest of all paths connecting the splitter & resolver
+        #any longer paths are extensions along the same split.
+        if resolver_paths:
+            shortest = sorted(resolver_paths, key = len)[0]
+            resolver = shortest[-1] #last node of the shortest path = resolver
+            up_edges = [(up, resolver) for up in G1.predecessors_iter(resolver)]
+            #tag the nodes and edges accordingly
+            G1.node[resolver]['up_splitter'] = splitter
+            G1.node[splitter]['dn_resolver'] = resolver
+            for u,v in up_edges:
+                G1[u][v]['flow_resolve'] = 'Y'
+                G1[u][v]['split_node'] = splitter
+
+        #record which segments are downstream of this node
+        dwn_edges = [(splitter, dn) for dn in G1.successors_iter(splitter)]
+        G1.node[splitter]['flow_split'] = splitter
+        G1.node[splitter]['flow_split_edges'] = dwn_edges
+
+        #tag the flow split sewers
+        total_capacity = max(sum([G1[u][v]['capacity'] for u,v in dwn_edges]),1)
+        for u,v in dwn_edges:
+            G1[u][v]['flow_split'] = 'Y'
+            if G1.in_degree(u)==0:
+                G1[u][v]['flow_split'] = 'summet'
+            G1[u][v]['flow_split_frac'] = G1[u][v]['capacity'] / total_capacity
 
 
-    return splitnodes
 
-def find_resolved_splits(G):
-    splitters = flow_splits(G)
-    results = {}
-    for s in splitters:
 
-        #look for descendants with more than one path between the splitter
-        s_degree =  G.out_degree(s) #out degree, i.e. how many paths its split into
-
-        resolve_paths = []
-        multi_in_ds = [d for d in nx.descendants(G, s) if G.in_degree(d) > 1]
-        SUB = nx.subgraph(G, nbunch=multi_in_ds)
-        for desc in nx.topological_sort(SUB, reverse=True):
-            paths = [p for p in nx.all_simple_paths(G, source=s, target=desc)]
-
-            if len(paths) > 1 and len(paths) == s_degree:
-                resolve_paths += paths
-                break
-
-        results.update({s:resolve_paths})
-
-    return results
+    return G1
