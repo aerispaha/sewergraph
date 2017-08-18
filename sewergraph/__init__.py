7dee600f0e244ab04a28e190917cf8066870a040
Adam Erispaha <aerispaha@gmail.com>
2017-08-18 19:36:08 -0400

    started improving in the spirit of osmnx

--- sewergraph/__init__.py
+++ sewergraph/__init__.py
@@ -1,1 +1,11 @@
-from sewergraph.trace import SewerGraph
+################################################################################
+# Module: __init__.py
+# Description: Tool for graph calculations on drainage networks
+# License: MIT, see full license in LICENSE.txt
+# Web: https://github.com/aerispaha/trace
+################################################################################
+
+from .core import *
+from .helpers import *
+
+__version__ = '0.0.4'
