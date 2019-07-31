ef5df1c3038fc492beccf196069e0e4be8165d5a
Adam Erispaha <aerispaha@gmail.com>
2019-07-31 14:09:03 -0400

    remove out of scope helper funcs

--- sewergraph/helpers.py
+++ sewergraph/helpers.py
@@ -107,30 +107,6 @@ def round_shapefile_node_keys(G):
     mapping = {n:tuple([round(i, 2) for i in n]) for n in G.nodes()}
     return nx.relabel_nodes(G, mapping)
 
-def clean_network_data(G):
-    """
-    remove unecessary fields from DataConv from the network, remove isolated
-    nodes
-    """
-    G1 = G.copy()
-
-    #remove isolated nodes
-    G1.remove_nodes_from(nx.isolates(G1))
-
-    for u,v,d in G1.edges(data=True):
-        node_keeper_keys = ['X_Coord', 'Y_Coord','cumulative_area',
-                            'local_area', 'facilityid', 'ELEVATION_', 'ELEVATIONI',
-                            'FacilityNa', 'RASTERVALU']
-        edge_keeper_keys = ['diameter', 'height','width', 'facilityid','Json',
-                            'slope', 'length', 'Year_Insta', 'pipeshape',
-                            'PIPE_TYPE', 'STICKERLIN', 'LABEL','ELEVATION_',
-                            'ELEVATIONI','slope_calculated',
-                            'slope_calculated_fids', 'local_area']
-        clean_dict(G1.node[u], node_keeper_keys)
-        clean_dict(G1.node[v], node_keeper_keys)
-        clean_dict(d, edge_keeper_keys)
-
-    return G1
 
 def pairwise(iterable):
     """
@@ -147,50 +123,3 @@ def random_alphanumeric(n=6):
 	import random
 	chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 	return ''.join(random.choice(chars) for i in range(n))
-
-
-def create_html_map(geo_layers, filename, G, basemap='mapbox_base.html'):
-    """
-    geo_layers: dict of dicts
-        dictionary of layers and their geojson data
-    """
-    import geojson
-
-    # This is the project root #HACK
-    ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    BASEMAP_PATH = os.path.join(ROOT_DIR,'basemaps',basemap)
-    # basemap_path = r'P:\06_Tools\sewertrace\basemaps\mapbox_base.html'
-
-    #get center point
-    gdf = gp.GeoDataFrame(nx.to_pandas_edgelist(G)[['geometry']])
-    hull = gdf.unary_union.convex_hull
-    c = (hull.centroid.x, hull.centroid.y)
-    bbox = hull.bounds
-
-    with open(BASEMAP_PATH, 'r') as bm:
-        # filename = os.path.join(os.path.dirname(geocondpath), self.alt_report.model.name + '.html')
-        with open(filename, 'wb') as newmap:
-            for line in bm:
-                if '//INSERT GEOJSON HERE ~~~~~' in line:
-                    for lyr, geodata in list(geo_layers.items()):
-                        jsondata = geojson.dumps(geodata)
-                        newmap.write('{} = {};\n'.format(lyr, jsondata))
-
-                    #write the network as a json object
-                    # net_dict = json_graph.node_link_data(G)
-                    edges = list([(u,v, {'facilityid':d}) for u,v,d in G.edges.data('facilityid')])
-                    nodes = list(G.nodes())
-
-                    # newmap.write('net_json = {};\n'.format(json.dumps(net_dict)))
-                    newmap.write('edges = {};\n'.format(json.dumps(edges)))
-                    newmap.write('nodes = {};\n'.format(json.dumps(nodes)))
-
-                if 'center: [-75.148946, 39.921685],' in line:
-                    newmap.write('center:[{}, {}],\n'.format(c[0], c[1]))
-                if '//INSERT BBOX HERE' in line:
-                    newmap.write('map.fitBounds([[{}, {}], [{}, {}]]);\n'
-                                 .format(bbox[0], bbox[1], bbox[2],
-                                         bbox[3]))
-
-                else:
-                    newmap.write(line)
